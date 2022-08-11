20357ff1f5d474f51e6d5f1a9d596b1a7cbf2f62
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-08-11 15:57:42 -0700

    created a homeimg component and added styling

---
+++ src/components/HomeImg.js
@@ -0,0 +1,24 @@
+import "./HomeImgStyles.css"
+import React from 'react'
+import LandingImg from "../assets/home-img.jpeg"
+import { Link } from "react-router-dom"
+
+function HomeImg() {
+  return (
+    <div className="home-img">
+        <div className="bckground">
+            <img className="landing-img" src={LandingImg} alt="LandingImg" />
+        </div>
+        <div className="content">
+            <p>Welcome, I'm Parker</p>
+            <h1>Full Stack Developer</h1>
+            <div>
+                <Link to="/project" className="btn">Projects</Link>
+                <Link to="/contact" className="btn btn-light">Contact</Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HomeImg
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import HomeImg from '../components/HomeImg'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <HomeImg />
     </div>
   )
 }
