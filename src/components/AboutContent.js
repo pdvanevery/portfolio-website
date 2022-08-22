c7657df27b3e682d5298117df596068c7f594f61
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-08-22 15:02:09 -0700

    added contact page form but no functionality

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -8,7 +8,7 @@ const AboutContent = () => {
     <div className="about">
         <div className="left">
             <h1>Who am I?</h1>
-            <p>My name is Parker and I am an aspiring Full Stack Developer.  I was born and raised in San Diego, CA, living there until 2016 which was when I then made the move up north to attend San Jose State University. While there, I graduated with my Bachelor's in Entrepreneurship as well as made San Jose my new home. </p>
+            <p>My name is Parker and I am an aspiring Full Stack Developer.  I was born and raised in San Diego, CA, living there until I left home to attend San Jose State University. While there, I graduated with my Bachelor's in Entrepreneurship as well as made San Jose my new home. </p>
                 
             <p>Shortly after graduating, I decided to go back to school to learn to code as it was a growing profession that had always been of interest to me, but had not pursued until now. After 10 months, or 473 hours of classes, not counting the countless hours spent studying, I am officially a graduate of San Diego State University's Professional Certificate Program for Software Development.   
             </p>
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import BackgroundImg from '../components/BackgroundImg'
+import Form from '../components/Form'
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
       <BackgroundImg heading="CONTACT" text="Let's chat"/>
+      <Form />
       <Footer />
     </div>
   )
