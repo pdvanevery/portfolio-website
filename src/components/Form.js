576235c0f2ad567a9f3aa88145e604eb5a9985a5
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-08-25 14:45:33 -0700

    small change to clean up forms.js code

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 import "./FormStyles.css"
 import React, { useRef } from 'react';
-import emailjs, { send } from '@emailjs/browser';
+import emailjs from '@emailjs/browser';
 
 const Form = () => {
 
