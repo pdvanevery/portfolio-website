50673558e3498445e6d0b81aa7d93153d260a144
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-08-25 14:39:28 -0700

    added contact form functionality and a temporary alert box when message is sent

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,34 @@
 import "./FormStyles.css"
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs, { send } from '@emailjs/browser';
 
 const Form = () => {
+
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault() //prevent refresh
+
+    emailjs.sendForm('service_ja76yzv', 'template_64b6kgg', form.current, 'ytfiAM8e8PjSbM0bu')
+    .then((result) => {
+      console.log(result.text)
+      console.log("message sent")
+      alert("Message sent!")
+    }, (error) => {
+      console.log(error.text)
+    })
+  }
+
   return (
     <div className="form">
-        <form>
+        <form ref={form} onSubmit={sendEmail}>
             <label>Your Name</label>
-            <input type="text" placeholder="Enter Name"></input>
+            <input type="text" name="user_name" placeholder="Enter Name"></input>
             <label>Email</label>
-            <input type="email" placeholder="Enter Email"></input>
-            <label>Subject</label>
-            <input type="text" placeholder="Enter Email Subject"></input>
+            <input type="email" name="user_email" placeholder="Enter Email"></input>
             <label>Message</label>
-            <textarea rows="6" placeholder="Enter Message Here" />
-            <button className="btn">Submit</button>
+            <textarea rows="6" name="message" placeholder="Enter Message Here" />
+            <button className="btn" type="submit" value="Send">Submit</button>
         </form>
     </div>
   )
